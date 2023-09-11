44e25866b94d07ed326d1d60aef3209c169ed827
AARON AQUINO <a20200705@idat.edu.pe>
2023-09-11 03:22:51 -0500

    add_clicked  method and text field with button.

--- To_Do_App.py
+++ To_Do_App.py
@@ -1,6 +1,12 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.add(ft.Text(value="Hola Mundo, Usando FLET", size=40, color="YELLOW",font_family="Segoe UI"))
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value=""
+        page.update()
+    
+    new_task = ft.TextField(hint_text="Whats needs to be done?")
+    page.add(new_task, ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked))
 
 ft.app(main)
